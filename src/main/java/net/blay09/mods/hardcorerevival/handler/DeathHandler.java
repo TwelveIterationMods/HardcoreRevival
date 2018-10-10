2a34efa75ef237b032410b0e351b253bf26f108f
blay09 <blay09@hotmail.de>
2018-10-10 20:44:25 +0200

    Fix players not always respawning at the correct position when being rescued
    #13

--- src/main/java/net/blay09/mods/hardcorerevival/handler/DeathHandler.java
+++ src/main/java/net/blay09/mods/hardcorerevival/handler/DeathHandler.java
@@ -5,7 +5,6 @@ import net.blay09.mods.hardcorerevival.ModConfig;
 import net.blay09.mods.hardcorerevival.PlayerKnockedOutEvent;
 import net.blay09.mods.hardcorerevival.capability.CapabilityHardcoreRevival;
 import net.blay09.mods.hardcorerevival.capability.IHardcoreRevival;
-import net.blay09.mods.hardcorerevival.network.MessageDeathTime;
 import net.blay09.mods.hardcorerevival.network.MessageDie;
 import net.blay09.mods.hardcorerevival.network.NetworkHandler;
 import net.minecraft.entity.player.EntityPlayer;
@@ -21,83 +20,90 @@ import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class DeathHandler {
-	public static final String IGNORE_REVIVAL_DEATH = "IgnoreRevivalDeath";
+    public static final String IGNORE_REVIVAL_DEATH = "IgnoreRevivalDeath";
 
-	@SubscribeEvent(priority = EventPriority.HIGHEST)
-	public void onPlayerDeath(LivingDeathEvent event) {
-		if (event.getEntityLiving() instanceof EntityPlayerMP) {
-			EntityPlayer player = (EntityPlayer) event.getEntityLiving();
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public void onPlayerDeath(LivingDeathEvent event) {
+        if (event.getEntityLiving() instanceof EntityPlayerMP) {
+            EntityPlayer player = (EntityPlayer) event.getEntityLiving();
 
-			// If the player fell into the void, there's no rescuing
-			if (event.getSource() == DamageSource.OUT_OF_WORLD) {
-				player.getEntityData().setBoolean(IGNORE_REVIVAL_DEATH, true);
-				NetworkHandler.instance.sendTo(new MessageDie(), (EntityPlayerMP) player);
-				return;
-			}
+            // If the player fell into the void, there's no rescuing
+            if (event.getSource() == DamageSource.OUT_OF_WORLD) {
+                player.getEntityData().setBoolean(IGNORE_REVIVAL_DEATH, true);
+                NetworkHandler.instance.sendTo(new MessageDie(), (EntityPlayerMP) player);
+                return;
+            }
 
-			// If IGNORE_REVIVAL_DEATH is set, this should be treated as a normal death
-			if (event.getSource() == HardcoreRevival.notRescuedInTime || player.getEntityData().getBoolean(IGNORE_REVIVAL_DEATH)) {
-				return;
-			}
+            // If IGNORE_REVIVAL_DEATH is set, this should be treated as a normal death
+            if (event.getSource() == HardcoreRevival.notRescuedInTime || player.getEntityData().getBoolean(IGNORE_REVIVAL_DEATH)) {
+                return;
+            }
 
-			// Fire event for compatibility addons
-			MinecraftForge.EVENT_BUS.post(new PlayerKnockedOutEvent(player, event.getSource()));
+            // Fire event for compatibility addons
+            MinecraftForge.EVENT_BUS.post(new PlayerKnockedOutEvent(player, event.getSource()));
 
-			// Dead players glow
-			if (ModConfig.glowOnDeath) {
-				player.setGlowing(true);
-			}
+            // Dead players glow
+            if (ModConfig.glowOnDeath) {
+                player.setGlowing(true);
+            }
 
-			// Cancel event - we're taking over from here
-			event.setCanceled(true);
+            // Cancel event - we're taking over from here
+            event.setCanceled(true);
 
-			// If enabled, show a death message
-			if (player.world.getGameRules().getBoolean("showDeathMessages")) {
-				MinecraftServer server = player.world.getMinecraftServer();
-				if (server != null) {
-					Team team = player.getTeam();
-					if (team != null && team.getDeathMessageVisibility() != Team.EnumVisible.ALWAYS) {
-						if (team.getDeathMessageVisibility() == Team.EnumVisible.HIDE_FOR_OTHER_TEAMS) {
-							server.getPlayerList().sendMessageToAllTeamMembers(player, player.getCombatTracker().getDeathMessage());
-						} else if (team.getDeathMessageVisibility() == Team.EnumVisible.HIDE_FOR_OWN_TEAM) {
-							server.getPlayerList().sendMessageToTeamOrAllPlayers(player, player.getCombatTracker().getDeathMessage());
-						}
-					} else {
-						server.getPlayerList().sendMessage(player.getCombatTracker().getDeathMessage());
-					}
-				}
-			}
-		}
-	}
+            // If enabled, show a death message
+            if (player.world.getGameRules().getBoolean("showDeathMessages")) {
+                MinecraftServer server = player.world.getMinecraftServer();
+                if (server != null) {
+                    Team team = player.getTeam();
+                    if (team != null && team.getDeathMessageVisibility() != Team.EnumVisible.ALWAYS) {
+                        if (team.getDeathMessageVisibility() == Team.EnumVisible.HIDE_FOR_OTHER_TEAMS) {
+                            server.getPlayerList().sendMessageToAllTeamMembers(player, player.getCombatTracker().getDeathMessage());
+                        } else if (team.getDeathMessageVisibility() == Team.EnumVisible.HIDE_FOR_OWN_TEAM) {
+                            server.getPlayerList().sendMessageToTeamOrAllPlayers(player, player.getCombatTracker().getDeathMessage());
+                        }
+                    } else {
+                        server.getPlayerList().sendMessage(player.getCombatTracker().getDeathMessage());
+                    }
+                }
+            }
+        }
+    }
 
-	@SubscribeEvent
-	public void onDeathUpdate(TickEvent.PlayerTickEvent event) {
-		if (event.phase == TickEvent.Phase.START) {
-			if (event.player.getHealth() <= 0f && !event.player.getEntityData().getBoolean(IGNORE_REVIVAL_DEATH)) {
-				// Prevent deathTime from removing the entity from the world
-				if (event.player.deathTime == 19) {
-					event.player.deathTime = 18;
-				}
-				// Update our death timer instead
-				IHardcoreRevival revival = event.player.getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null);
-				if (revival != null) {
-					revival.setDeathTime(revival.getDeathTime() + 1);
-					if (revival.getDeathTime() >= ModConfig.maxDeathTicks) {
-						event.player.getEntityData().setBoolean(IGNORE_REVIVAL_DEATH, true);
-						NetworkHandler.instance.sendTo(new MessageDie(), (EntityPlayerMP) event.player);
-						event.player.getCombatTracker().trackDamage(HardcoreRevival.notRescuedInTime, 0, 0);
-						event.player.onDeath(HardcoreRevival.notRescuedInTime);
-					}
-				}
-			}
-		}
-	}
+    @SubscribeEvent
+    public void onDeathUpdate(TickEvent.PlayerTickEvent event) {
+        if (event.phase == TickEvent.Phase.START) {
+            if (event.player.getHealth() <= 0f && !event.player.getEntityData().getBoolean(IGNORE_REVIVAL_DEATH)) {
+                // Prevent deathTime from removing the entity from the world
+                if (event.player.deathTime == 19) {
+                    event.player.deathTime = 18;
+                }
+                // Update our death timer instead
+                IHardcoreRevival revival = event.player.getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null);
+                if (revival != null) {
+                    revival.setDeathTime(revival.getDeathTime() + 1);
+                    if (revival.getDeathTime() >= ModConfig.maxDeathTicks) {
+                        event.player.getEntityData().setBoolean(IGNORE_REVIVAL_DEATH, true);
+                        NetworkHandler.instance.sendTo(new MessageDie(), (EntityPlayerMP) event.player);
+                        event.player.getCombatTracker().trackDamage(HardcoreRevival.notRescuedInTime, 0, 0);
+                        event.player.onDeath(HardcoreRevival.notRescuedInTime);
+                        revival.setDeathTime(0);
+                    }
+                }
+            }
+        }
+    }
 
-	@SubscribeEvent
-	public void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event) {
-		event.player.getEntityData().removeTag(IGNORE_REVIVAL_DEATH);
-		if (ModConfig.glowOnDeath) {
-			event.player.setGlowing(false);
-		}
-	}
+    @SubscribeEvent
+    public void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event) {
+        event.player.getEntityData().removeTag(IGNORE_REVIVAL_DEATH);
+
+        if (ModConfig.glowOnDeath) {
+            event.player.setGlowing(false);
+        }
+
+        IHardcoreRevival revival = event.player.getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null);
+        if (revival != null) {
+            revival.setDeathTime(0);
+        }
+    }
 }
--- src/main/java/net/blay09/mods/hardcorerevival/handler/RescueHandler.java
+++ src/main/java/net/blay09/mods/hardcorerevival/handler/RescueHandler.java
@@ -14,6 +14,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
@@ -21,99 +22,114 @@ import net.minecraftforge.fml.relauncher.Side;
 
 public class RescueHandler {
 
-	@SubscribeEvent
-	public void onItemUse(LivingEntityUseItemEvent event) {
-		if (event.getEntityLiving() instanceof EntityPlayer) {
-			// Stop rescuing if the player does something other than rescuing
-			abortRescue((EntityPlayer) event.getEntityLiving());
-		}
-	}
+    @SubscribeEvent
+    public void onItemUse(LivingEntityUseItemEvent event) {
+        if (event.getEntityLiving() instanceof EntityPlayer) {
+            // Stop rescuing if the player does something other than rescuing
+            abortRescue((EntityPlayer) event.getEntityLiving());
+        }
+    }
 
-	@SubscribeEvent
-	public void onAttack(AttackEntityEvent event) {
-		// Stop rescuing if the player does something other than rescuing
-		abortRescue(event.getEntityPlayer());
-	}
+    @SubscribeEvent
+    public void onAttack(AttackEntityEvent event) {
+        // Stop rescuing if the player does something other than rescuing
+        abortRescue(event.getEntityPlayer());
+    }
 
-	public static void startRescue(EntityPlayer player, EntityPlayer target) {
-		IHardcoreRevival revival = player.getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null);
-		if (revival != null) {
-			revival.setRescueTarget(target);
-			revival.setRescueTime(0);
-			NetworkHandler.instance.sendTo(new MessageRevivalProgress(target.getEntityId(), 0f), (EntityPlayerMP) player);
-		}
-	}
+    public static void startRescue(EntityPlayer player, EntityPlayer target) {
+        IHardcoreRevival revival = player.getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null);
+        if (revival != null) {
+            revival.setRescueTarget(target);
+            revival.setRescueTime(0);
+            NetworkHandler.instance.sendTo(new MessageRevivalProgress(target.getEntityId(), 0f), (EntityPlayerMP) player);
+        }
+    }
 
-	@SubscribeEvent
-	public void onPlayerTick(TickEvent.PlayerTickEvent event) {
-		if (event.side == Side.SERVER && event.phase == TickEvent.Phase.END) {
-			IHardcoreRevival revival = event.player.getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null);
-			if (revival != null && revival.getRescueTarget() != null) {
-				// Stop rescuing if the target logged out
-				if (revival.getRescueTarget().isDead) {
-					abortRescue(event.player);
-				} else {
-					// Stop rescuing if the player is out of range
-					float dist = event.player.getDistanceToEntity(revival.getRescueTarget());
-					if (dist > ModConfig.maxRescueDist) {
-						abortRescue(event.player);
-					} else {
-						int rescueTime = revival.getRescueTime() + 1;
-						revival.setRescueTime(rescueTime);
-						int step = ModConfig.rescueTime / 4;
-						if (rescueTime >= ModConfig.rescueTime) {
-							finishRescue(event.player);
-						} else if (rescueTime % step == 0) {
-							NetworkHandler.instance.sendTo(new MessageRevivalProgress(revival.getRescueTarget().getEntityId(), (float) rescueTime / (float) ModConfig.rescueTime), (EntityPlayerMP) event.player);
-						}
-					}
-				}
-			}
-		}
-	}
+    @SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.side == Side.SERVER && event.phase == TickEvent.Phase.END) {
+            IHardcoreRevival revival = event.player.getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null);
+            if (revival != null && revival.getRescueTarget() != null) {
+                // Stop rescuing if the target logged out
+                if (revival.getRescueTarget().isDead) {
+                    abortRescue(event.player);
+                } else {
+                    // Stop rescuing if the player is out of range
+                    float dist = event.player.getDistanceToEntity(revival.getRescueTarget());
+                    if (dist > ModConfig.maxRescueDist) {
+                        abortRescue(event.player);
+                    } else {
+                        int rescueTime = revival.getRescueTime() + 1;
+                        revival.setRescueTime(rescueTime);
+                        int step = ModConfig.rescueTime / 4;
+                        if (rescueTime >= ModConfig.rescueTime) {
+                            finishRescue(event.player);
+                        } else if (rescueTime % step == 0) {
+                            NetworkHandler.instance.sendTo(new MessageRevivalProgress(revival.getRescueTarget().getEntityId(), (float) rescueTime / (float) ModConfig.rescueTime), (EntityPlayerMP) event.player);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-	public static void finishRescue(EntityPlayer player) {
-		IHardcoreRevival revival = player.getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null);
-		if (revival != null) {
-			EntityPlayer target = revival.getRescueTarget();
-			if (target != null) {
-				MinecraftServer server = target.getServer();
-				if (server != null) {
-					BlockPos prevSpawnPos = target.getBedLocation(target.dimension);
-					boolean prevSpawnForced = target.isSpawnForced(target.dimension);
-					target.setSpawnPoint(target.getPosition(), true);
-					if (ModConfig.glowOnDeath) {
-						target.setGlowing(false);
-					}
-					EntityPlayerMP newPlayer = server.getPlayerList().recreatePlayerEntity((EntityPlayerMP) target, target.dimension, false);
-					((EntityPlayerMP) target).connection.player = newPlayer;
-					newPlayer.setHealth(1f);
-					newPlayer.getFoodStats().setFoodLevel(5);
-					newPlayer.addPotionEffect(new PotionEffect(MobEffects.HUNGER, 20 * 30));
-					newPlayer.addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, 20 * 60));
-					newPlayer.inventory.copyInventory(target.inventory);
-					newPlayer.experienceLevel = target.experienceLevel;
-					newPlayer.experienceTotal = target.experienceTotal;
-					newPlayer.experience = target.experience;
+    @SubscribeEvent
+    public void onPlayerClone(PlayerEvent.Clone event) {
+        EntityPlayer original = event.getOriginal();
+        IHardcoreRevival revival = original.getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null);
+        if (revival != null && revival.getDeathTime() > 0) {
+            event.getEntityPlayer().setLocationAndAngles(original.posX, original.posY, original.posZ, 0f, 0f);
+        }
+    }
 
-					newPlayer.extinguish();
-					newPlayer.setFlag(0, false); // burning flag
+    public static void finishRescue(EntityPlayer player) {
+        IHardcoreRevival revival = player.getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null);
+        if (revival != null) {
+            EntityPlayer target = revival.getRescueTarget();
+            if (target != null) {
+                MinecraftServer server = target.getServer();
+                if (server != null) {
+                    // Remember the old spawn point and then disable it so we can manually position the player
+                    BlockPos prevSpawnPos = target.getBedLocation(target.dimension);
+                    boolean prevSpawnForced = target.isSpawnForced(target.dimension);
+                    target.setSpawnChunk(null, false, target.dimension);
 
-					newPlayer.setScore(target.getScore());
-					newPlayer.setSpawnPoint(prevSpawnPos, prevSpawnForced);
-					NetworkHandler.instance.sendToAllAround(new MessageRevivalSuccess(newPlayer.getEntityId()), new NetworkRegistry.TargetPoint(newPlayer.dimension, newPlayer.posX, newPlayer.posY, newPlayer.posZ, 32));
-				}
-			}
-		}
-	}
+                    if (ModConfig.glowOnDeath) {
+                        target.setGlowing(false);
+                    }
 
-	public static void abortRescue(EntityPlayer player) {
-		IHardcoreRevival revival = player.getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null);
-		if (revival != null) {
-			revival.setRescueTime(0);
-			revival.setRescueTarget(null);
-			NetworkHandler.instance.sendTo(new MessageRevivalProgress(-1, -1), (EntityPlayerMP) player);
-		}
-	}
+                    EntityPlayerMP newPlayer = server.getPlayerList().recreatePlayerEntity((EntityPlayerMP) target, target.dimension, false);
+                    ((EntityPlayerMP) target).connection.player = newPlayer;
+                    newPlayer.setHealth(1f);
+                    newPlayer.getFoodStats().setFoodLevel(5);
+                    newPlayer.addPotionEffect(new PotionEffect(MobEffects.HUNGER, 20 * 30));
+                    newPlayer.addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, 20 * 60));
+                    newPlayer.inventory.copyInventory(target.inventory);
+                    newPlayer.experienceLevel = target.experienceLevel;
+                    newPlayer.experienceTotal = target.experienceTotal;
+                    newPlayer.experience = target.experience;
+
+                    newPlayer.extinguish();
+                    newPlayer.setFlag(0, false); // burning flag
+
+                    newPlayer.setScore(target.getScore());
+
+                    // Restore the old spawnpoint
+                    newPlayer.setSpawnPoint(prevSpawnPos, prevSpawnForced);
+
+                    NetworkHandler.instance.sendToAllAround(new MessageRevivalSuccess(newPlayer.getEntityId()), new NetworkRegistry.TargetPoint(newPlayer.dimension, newPlayer.posX, newPlayer.posY, newPlayer.posZ, 32));
+                }
+            }
+        }
+    }
+
+    public static void abortRescue(EntityPlayer player) {
+        IHardcoreRevival revival = player.getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null);
+        if (revival != null) {
+            revival.setRescueTime(0);
+            revival.setRescueTarget(null);
+            NetworkHandler.instance.sendTo(new MessageRevivalProgress(-1, -1), (EntityPlayerMP) player);
+        }
+    }
 
 }
