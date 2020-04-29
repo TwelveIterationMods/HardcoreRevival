71f2de5a8ab9411e9ef94b8b1f646d24720840e5
blay09 <blay09@hotmail.de>
2020-04-29 18:46:13 +0200

    Fix weird race condition when dieing in void or to /kill command
    #34

--- src/main/java/net/blay09/mods/hardcorerevival/handler/DeathHandler.java
+++ src/main/java/net/blay09/mods/hardcorerevival/handler/DeathHandler.java
@@ -1,5 +1,6 @@
 package net.blay09.mods.hardcorerevival.handler;
 
+
 import net.blay09.mods.hardcorerevival.HardcoreRevival;
 import net.blay09.mods.hardcorerevival.HardcoreRevivalConfig;
 import net.blay09.mods.hardcorerevival.PlayerKnockedOutEvent;
@@ -22,50 +23,55 @@ import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.network.PacketDistributor;
 
-public class DeathHandler {
-    public static final String IGNORE_REVIVAL_DEATH = "IgnoreRevivalDeath";
 
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public void onPlayerDeath(LivingDeathEvent event) {
-        if (event.getEntityLiving() instanceof ServerPlayerEntity) {
+public class DeathHandler {
+
+    public static final String IGNORE_REVIVAL_DEATH = "IgnoreRevivalDeath";
+    public static final String DIED_OUT_OF_WORLD = "DiedOutOfWorld";
+
+    @SubscribeEvent( priority = EventPriority.HIGHEST )
+    public void onPlayerDeath( LivingDeathEvent event ) {
+        if( event.getEntityLiving() instanceof ServerPlayerEntity ) {
             PlayerEntity player = (PlayerEntity) event.getEntityLiving();
 
             // If the player fell into the void, there's no rescuing
-            if (event.getSource() == DamageSource.OUT_OF_WORLD) {
-                player.getPersistentData().putBoolean(IGNORE_REVIVAL_DEATH, true);
-                NetworkHandler.channel.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) player), new MessageDie());
+            if( event.getSource() == DamageSource.OUT_OF_WORLD ) {
+                player.getPersistentData().putBoolean( IGNORE_REVIVAL_DEATH, true );
+                player.getPersistentData().putBoolean( DIED_OUT_OF_WORLD, true );
                 return;
             }
 
             // If IGNORE_REVIVAL_DEATH is set, this should be treated as a normal death
-            if (event.getSource() == HardcoreRevival.notRescuedInTime || player.getPersistentData().getBoolean(IGNORE_REVIVAL_DEATH)) {
+            if( event.getSource() == HardcoreRevival.notRescuedInTime || player.getPersistentData().getBoolean( IGNORE_REVIVAL_DEATH ) ) {
                 return;
             }
 
             // Fire event for compatibility addons
-            MinecraftForge.EVENT_BUS.post(new PlayerKnockedOutEvent(player, event.getSource()));
+            MinecraftForge.EVENT_BUS.post( new PlayerKnockedOutEvent( player, event.getSource() ) );
 
             // Dead players glow
-            if (HardcoreRevivalConfig.SERVER.glowOnDeath.get()) {
-                player.setGlowing(true);
+            if( HardcoreRevivalConfig.SERVER.glowOnDeath.get() ) {
+                player.setGlowing( true );
             }
 
             // Cancel event - we're taking over from here
-            event.setCanceled(true);
+            event.setCanceled( true );
 
             // If enabled, show a death message
-            if (player.world.getGameRules().getBoolean(GameRules.SHOW_DEATH_MESSAGES)) {
+            if( player.world.getGameRules().getBoolean( GameRules.SHOW_DEATH_MESSAGES ) ) {
                 MinecraftServer server = player.world.getServer();
-                if (server != null) {
+                if( server != null ) {
                     Team team = player.getTeam();
-                    if (team != null && team.getDeathMessageVisibility() != Team.Visible.ALWAYS) {
-                        if (team.getDeathMessageVisibility() == Team.Visible.HIDE_FOR_OTHER_TEAMS) {
-                            server.getPlayerList().sendMessageToAllTeamMembers(player, player.getCombatTracker().getDeathMessage());
-                        } else if (team.getDeathMessageVisibility() == Team.Visible.HIDE_FOR_OWN_TEAM) {
-                            server.getPlayerList().sendMessageToTeamOrAllPlayers(player, player.getCombatTracker().getDeathMessage());
+                    if( team != null && team.getDeathMessageVisibility() != Team.Visible.ALWAYS ) {
+                        if( team.getDeathMessageVisibility() == Team.Visible.HIDE_FOR_OTHER_TEAMS ) {
+                            server.getPlayerList().sendMessageToAllTeamMembers( player, player.getCombatTracker().getDeathMessage() );
                         }
-                    } else {
-                        server.getPlayerList().sendMessage(player.getCombatTracker().getDeathMessage());
+                        else if( team.getDeathMessageVisibility() == Team.Visible.HIDE_FOR_OWN_TEAM ) {
+                            server.getPlayerList().sendMessageToTeamOrAllPlayers( player, player.getCombatTracker().getDeathMessage() );
+                        }
+                    }
+                    else {
+                        server.getPlayerList().sendMessage( player.getCombatTracker().getDeathMessage() );
                     }
                 }
             }
@@ -73,42 +79,48 @@ public class DeathHandler {
     }
 
     @SubscribeEvent
-    public void onDeathUpdate(TickEvent.PlayerTickEvent event) {
-        if (event.phase == TickEvent.Phase.START) {
-            if (event.player.getHealth() <= 0f && !event.player.getPersistentData().getBoolean(IGNORE_REVIVAL_DEATH)) {
+    public void onDeathUpdate( TickEvent.PlayerTickEvent event ) {
+        if( event.phase == TickEvent.Phase.START ) {
+            // Send death packet for out-of-world deaths on next tick to prevent race condition between death screen and client-side revival screen
+            if( event.player.getPersistentData().getBoolean( DIED_OUT_OF_WORLD ) ) {
+                NetworkHandler.channel.send( PacketDistributor.PLAYER.with( () -> (ServerPlayerEntity) event.player ), new MessageDie() );
+                event.player.getPersistentData().remove( DIED_OUT_OF_WORLD );
+            }
+
+            if( event.player.getHealth() <= 0f && !event.player.getPersistentData().getBoolean( IGNORE_REVIVAL_DEATH ) ) {
                 // Prevent deathTime from removing the entity from the world
-                if (event.player.deathTime == 19) {
+                if( event.player.deathTime == 19 ) {
                     event.player.deathTime = 18;
                 }
                 // Update our death timer instead
-                LazyOptional<IHardcoreRevival> revival = event.player.getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY);
-                revival.ifPresent(it -> {
-                    it.setDeathTime(it.getDeathTime() + 1);
-                    if (!HardcoreRevivalConfig.SERVER.disableDeathTimer.get() && it.getDeathTime() >= HardcoreRevivalConfig.SERVER.maxDeathTicks.get()) {
-                        finalDeath(event.player, it);
+                LazyOptional<IHardcoreRevival> revival = event.player.getCapability( CapabilityHardcoreRevival.REVIVAL_CAPABILITY );
+                revival.ifPresent( it -> {
+                    it.setDeathTime( it.getDeathTime() + 1 );
+                    if( !HardcoreRevivalConfig.SERVER.disableDeathTimer.get() && it.getDeathTime() >= HardcoreRevivalConfig.SERVER.maxDeathTicks.get() ) {
+                        finalDeath( event.player, it );
                     }
-                });
+                } );
             }
         }
     }
 
-    public static void finalDeath(PlayerEntity player, IHardcoreRevival it) {
-        player.getPersistentData().putBoolean(IGNORE_REVIVAL_DEATH, true);
-        NetworkHandler.channel.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) player), new MessageDie());
-        player.getCombatTracker().trackDamage(HardcoreRevival.notRescuedInTime, 0, 0);
-        player.onDeath(HardcoreRevival.notRescuedInTime);
-        it.setDeathTime(0);
+    public static void finalDeath( PlayerEntity player, IHardcoreRevival it ) {
+        player.getPersistentData().putBoolean( IGNORE_REVIVAL_DEATH, true );
+        NetworkHandler.channel.send( PacketDistributor.PLAYER.with( () -> (ServerPlayerEntity) player ), new MessageDie() );
+        player.getCombatTracker().trackDamage( HardcoreRevival.notRescuedInTime, 0, 0 );
+        player.onDeath( HardcoreRevival.notRescuedInTime );
+        it.setDeathTime( 0 );
     }
 
     @SubscribeEvent
-    public void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event) {
-        event.getPlayer().getPersistentData().remove(IGNORE_REVIVAL_DEATH);
+    public void onPlayerRespawn( PlayerEvent.PlayerRespawnEvent event ) {
+        event.getPlayer().getPersistentData().remove( IGNORE_REVIVAL_DEATH );
 
-        if (HardcoreRevivalConfig.SERVER.glowOnDeath.get()) {
-            event.getPlayer().setGlowing(false);
+        if( HardcoreRevivalConfig.SERVER.glowOnDeath.get() ) {
+            event.getPlayer().setGlowing( false );
         }
 
-        LazyOptional<IHardcoreRevival> revival = event.getPlayer().getCapability(CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null);
-        revival.ifPresent(it -> it.setDeathTime(0));
+        LazyOptional<IHardcoreRevival> revival = event.getPlayer().getCapability( CapabilityHardcoreRevival.REVIVAL_CAPABILITY, null );
+        revival.ifPresent( it -> it.setDeathTime( 0 ) );
     }
 }
