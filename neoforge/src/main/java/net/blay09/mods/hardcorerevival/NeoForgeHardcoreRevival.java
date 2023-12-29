b6919e0f7274abe2a0861d1245ebe3b557139787
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-12-29 10:05:31 +0100

    fix(1.20.4): Update for NeoForge capability changes

--- neoforge/src/main/java/net/blay09/mods/hardcorerevival/NeoForgeHardcoreRevival.java
+++ neoforge/src/main/java/net/blay09/mods/hardcorerevival/NeoForgeHardcoreRevival.java
@@ -6,61 +6,32 @@ import net.blay09.mods.balm.neoforge.provider.NeoForgeBalmProviders;
 import net.blay09.mods.hardcorerevival.capability.HardcoreRevivalData;
 import net.blay09.mods.hardcorerevival.capability.HardcoreRevivalDataImpl;
 import net.blay09.mods.hardcorerevival.client.HardcoreRevivalClient;
-import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.EntityType;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.DistExecutor;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.common.capabilities.*;
-import net.neoforged.neoforge.common.util.LazyOptional;
-import net.neoforged.neoforge.event.AttachCapabilitiesEvent;
-
-import javax.annotation.Nullable;
+import net.neoforged.neoforge.capabilities.EntityCapability;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 
 @Mod(HardcoreRevival.MOD_ID)
 public class NeoForgeHardcoreRevival {
 
-    private final Capability<HardcoreRevivalData> hardcoreRevivalDataCapability = CapabilityManager.get(new CapabilityToken<>() {
-    });
+    private final EntityCapability<HardcoreRevivalData, Void> hardcoreRevivalDataCapability = EntityCapability.createVoid(new ResourceLocation(HardcoreRevival.MOD_ID,
+            "entity_data"), HardcoreRevivalData.class);
 
-    public NeoForgeHardcoreRevival() {
+    public NeoForgeHardcoreRevival(IEventBus eventBus) {
         Balm.initialize(HardcoreRevival.MOD_ID, HardcoreRevival::initialize);
         DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(HardcoreRevival.MOD_ID, HardcoreRevivalClient::initialize));
 
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::registerCapabilities);
-        NeoForge.EVENT_BUS.addGenericListener(Entity.class, this::attachEntityCapabilities);
+        eventBus.addListener(this::registerCapabilities);
 
         NeoForgeBalmProviders providers = (NeoForgeBalmProviders) Balm.getProviders();
-        providers.register(HardcoreRevivalData.class, new CapabilityToken<>() {
-        });
+        providers.registerEntityProvider(HardcoreRevivalData.class, hardcoreRevivalDataCapability);
     }
 
     private void registerCapabilities(RegisterCapabilitiesEvent event) {
-        event.register(HardcoreRevivalData.class);
-    }
-
-    private void attachEntityCapabilities(AttachCapabilitiesEvent<Entity> event) {
-        if (event.getObject() instanceof Player) {
-            event.addCapability(new ResourceLocation(HardcoreRevival.MOD_ID, "entity_data"), new ICapabilityProvider() {
-                private LazyOptional<HardcoreRevivalData> revival;
-
-                private LazyOptional<HardcoreRevivalData> getRevivalCapabilityInstance() {
-                    if (revival == null) {
-                        revival = LazyOptional.of(HardcoreRevivalDataImpl::new);
-                    }
-
-                    return revival;
-                }
-
-                @Override
-                public <T> LazyOptional<T> getCapability(Capability<T> cap, @Nullable Direction facing) {
-                    return hardcoreRevivalDataCapability.orEmpty(cap, getRevivalCapabilityInstance());
-                }
-            });
-        }
+        event.registerEntity(hardcoreRevivalDataCapability, EntityType.PLAYER, (player, context) -> new HardcoreRevivalDataImpl());
     }
 }
