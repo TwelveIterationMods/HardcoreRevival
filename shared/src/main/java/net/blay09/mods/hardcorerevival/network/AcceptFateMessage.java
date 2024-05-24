82d7aa504d6e848966e7fa9775478ef087002085
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-24 13:00:19 +0200

    feat: Add allowAcceptingFate option to remove the Accept your Fate button #125

--- shared/src/main/java/net/blay09/mods/hardcorerevival/network/AcceptFateMessage.java
+++ shared/src/main/java/net/blay09/mods/hardcorerevival/network/AcceptFateMessage.java
@@ -1,6 +1,7 @@
 package net.blay09.mods.hardcorerevival.network;
 
 import net.blay09.mods.hardcorerevival.HardcoreRevival;
+import net.blay09.mods.hardcorerevival.config.HardcoreRevivalConfig;
 import net.minecraft.server.level.ServerPlayer;
 
 public class AcceptFateMessage {
@@ -8,6 +9,10 @@ public class AcceptFateMessage {
     private boolean dummy;
 
     public static void handle(ServerPlayer player, AcceptFateMessage message) {
+        if (!HardcoreRevivalConfig.getActive().allowAcceptingFate) {
+            return;
+        }
+
         HardcoreRevival.getManager().notRescuedInTime(player);
     }
 
