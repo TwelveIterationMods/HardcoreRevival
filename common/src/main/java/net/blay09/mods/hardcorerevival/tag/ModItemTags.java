388eac259918d4753955a7a0f223ff45283f766b
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-07-25 09:28:33 +0200

    feat: Add allow_*_while_knocked_out tags for attack, use, toss (item) and use, break (block)

---
+++ common/src/main/java/net/blay09/mods/hardcorerevival/tag/ModItemTags.java
@@ -0,0 +1,13 @@
+package net.blay09.mods.hardcorerevival.tag;
+
+import net.blay09.mods.hardcorerevival.HardcoreRevival;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+public class ModItemTags {
+    public static final TagKey<Item> ALLOW_ATTACK_WHILE_KNOCKED_OUT = TagKey.create(Registries.ITEM, new ResourceLocation(HardcoreRevival.MOD_ID, "allow_attack_while_knocked_out"));
+    public static final TagKey<Item> ALLOW_USE_WHILE_KNOCKED_OUT = TagKey.create(Registries.ITEM, new ResourceLocation(HardcoreRevival.MOD_ID, "allow_use_while_knocked_out"));
+    public static final TagKey<Item> ALLOW_TOSS_WHILE_KNOCKED_OUT = TagKey.create(Registries.ITEM, new ResourceLocation(HardcoreRevival.MOD_ID, "allow_toss_while_knocked_out"));
+}
