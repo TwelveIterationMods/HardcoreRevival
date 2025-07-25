1e1cce21e887827c8836624691daa46cf07dd971
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-07-25 09:57:18 +0200

    refactor: Rewrite item toss restriction to be more stable

--- common/src/main/java/net/blay09/mods/hardcorerevival/mixin/ServerGamePacketListenerImplMixin.java
+++ common/src/main/java/net/blay09/mods/hardcorerevival/mixin/ServerGamePacketListenerImplMixin.java
@@ -2,6 +2,7 @@ package net.blay09.mods.hardcorerevival.mixin;
 
 import net.blay09.mods.hardcorerevival.MixinHooks;
 import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
+import net.minecraft.network.protocol.game.ServerboundPlayerActionPacket;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerGamePacketListenerImpl.class)
-public class ServerPlayNetHandlerMixin {
+public class ServerGamePacketListenerImplMixin {
 
     @Inject(method = "handleMovePlayer(Lnet/minecraft/network/protocol/game/ServerboundMovePlayerPacket;)V", at = @At("HEAD"), cancellable = true)
     public void handleMovePlayer(ServerboundMovePlayerPacket packet, CallbackInfo ci) {
@@ -17,6 +18,23 @@ public class ServerPlayNetHandlerMixin {
         if (MixinHooks.shouldCancelMovement(netHandler.player)) {
             MixinHooks.handleProcessPlayerRotation(netHandler.player, packet);
             ci.cancel();
+        }
+    }
+
+    @Inject(method = "handlePlayerAction(Lnet/minecraft/network/protocol/game/ServerboundPlayerActionPacket;)V", at = @At("HEAD"), cancellable = true)
+    public void handlePlayerAction(ServerboundPlayerActionPacket packet, CallbackInfo ci) {
+        final var netHandler = (ServerGamePacketListenerImpl) (Object) this;
+        switch (packet.getAction()) {
+            case DROP_ALL_ITEMS:
+                if (MixinHooks.shouldCancelTossAll(netHandler.player)) {
+                    ci.cancel();
+                }
+                break;
+            case DROP_ITEM:
+                if (MixinHooks.shouldCancelToss(netHandler.player, netHandler.player.getInventory().getSelected())) {
+                    ci.cancel();
+                }
+                break;
         }
     }
 
