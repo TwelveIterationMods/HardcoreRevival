30242465483639a128b807ac4cae821187a0cba3
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-07-25 09:28:33 +0200

    feat: Add allow_*_while_knocked_out tags for attack, use, toss (item) and use, break (block)

--- common/src/main/java/net/blay09/mods/hardcorerevival/handler/KnockoutRestrictionHandler.java
+++ common/src/main/java/net/blay09/mods/hardcorerevival/handler/KnockoutRestrictionHandler.java
@@ -4,10 +4,15 @@ import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.api.event.*;
 import net.blay09.mods.hardcorerevival.HardcoreRevival;
 import net.blay09.mods.hardcorerevival.config.HardcoreRevivalConfig;
+import net.blay09.mods.hardcorerevival.tag.ModBlockTags;
+import net.blay09.mods.hardcorerevival.tag.ModItemTags;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class KnockoutRestrictionHandler {
 
@@ -31,8 +36,10 @@ public class KnockoutRestrictionHandler {
     public static void onDigSpeed(DigSpeedEvent event) {
         Player player = event.getPlayer();
         if (player != null && HardcoreRevival.getRevivalData(player).isKnockedOut()) {
-            event.setSpeedOverride(0f);
-            event.setCanceled(true);
+            if (!mayBreakBlockKnockedOut(event.getState())) {
+                event.setSpeedOverride(0f);
+                event.setCanceled(true);
+            }
         }
     }
 
@@ -40,7 +47,9 @@ public class KnockoutRestrictionHandler {
         Player player = event.getPlayer();
         if (HardcoreRevival.getRevivalData(player).isKnockedOut()) {
             ItemStack itemStack = player.getItemInHand(event.getHand());
-            if (!HardcoreRevivalConfig.getActive().allowBows || !(itemStack.getItem() instanceof BowItem)) {
+            final var level = event.getLevel();
+            final var pos = event.getHitResult().getBlockPos();
+            if (!mayUseItemKnockedOut(itemStack) && !mayUseBlockKnockedOut(level, pos, itemStack)) {
                 event.setCanceled(true);
             }
         }
@@ -50,7 +59,7 @@ public class KnockoutRestrictionHandler {
         LivingEntity player = event.getPlayer();
         if (HardcoreRevival.getRevivalData(player).isKnockedOut()) {
             ItemStack itemStack = player.getItemInHand(event.getHand());
-            if (!HardcoreRevivalConfig.getActive().allowBows || !(itemStack.getItem() instanceof BowItem)) {
+            if (!mayUseItemKnockedOut(itemStack)) {
                 event.setCanceled(true);
             }
         }
@@ -59,9 +68,11 @@ public class KnockoutRestrictionHandler {
     public static void onTossItem(TossItemEvent event) {
         Player player = event.getPlayer();
         if (HardcoreRevival.getRevivalData(player).isKnockedOut()) {
-            // We try to suppress the drop on the client too, but if that failed for some reason, just try to revert the action
-            if (player.addItem(event.getItemStack())) {
-                event.setCanceled(true);
+            if (!mayTossItemKnockedOut(event.getItemStack())) {
+                // We try to suppress the drop on the client too, but if that failed for some reason, just try to revert the action
+                if (player.addItem(event.getItemStack())) {
+                    event.setCanceled(true);
+                }
             }
         }
     }
@@ -69,11 +80,42 @@ public class KnockoutRestrictionHandler {
     public static void onAttack(PlayerAttackEvent event) {
         Player player = event.getPlayer();
         if (player != null && HardcoreRevival.getRevivalData(player).isKnockedOut()) {
-            if (HardcoreRevivalConfig.getActive().allowUnarmedMelee && player.getMainHandItem().isEmpty()) {
+            final var itemStack = player.getMainHandItem();
+            if (HardcoreRevivalConfig.getActive().allowUnarmedMelee && itemStack.isEmpty()) {
                 return;
             }
 
-            event.setCanceled(true);
+            if (!mayAttackWithItemKnockedOut(itemStack)) {
+                event.setCanceled(true);
+            }
         }
+    }
+
+    private static boolean mayUseItemKnockedOut(ItemStack itemStack) {
+        if (HardcoreRevivalConfig.getActive().allowBows && (itemStack.getItem() instanceof BowItem)) {
+            return true;
+        }
+
+        return itemStack.is(ModItemTags.ALLOW_USE_WHILE_KNOCKED_OUT);
+    }
+
+    private static boolean mayTossItemKnockedOut(ItemStack itemStack) {
+        return itemStack.is(ModItemTags.ALLOW_TOSS_WHILE_KNOCKED_OUT);
+    }
+
+    private static boolean mayAttackWithItemKnockedOut(ItemStack itemStack) {
+        return itemStack.is(ModItemTags.ALLOW_ATTACK_WHILE_KNOCKED_OUT);
+    }
+
+    private static boolean mayUseBlockKnockedOut(Level level, BlockPos pos, ItemStack itemStack) {
+        if (!itemStack.isEmpty()) {
+            return false;
+        }
+
+        return level.getBlockState(pos).is(ModBlockTags.ALLOW_USE_WHILE_KNOCKED_OUT);
+    }
+
+    private static boolean mayBreakBlockKnockedOut(BlockState state) {
+        return state.is(ModBlockTags.ALLOW_BREAK_WHILE_KNOCKED_OUT);
     }
 }
---
+++ common/src/main/java/net/blay09/mods/hardcorerevival/tag/ModBlockTags.java
@@ -0,0 +1,13 @@
+package net.blay09.mods.hardcorerevival.tag;
+
+import net.blay09.mods.hardcorerevival.HardcoreRevival;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModBlockTags {
+    public static final TagKey<Block> ALLOW_USE_WHILE_KNOCKED_OUT = TagKey.create(Registries.BLOCK, new ResourceLocation(HardcoreRevival.MOD_ID, "allow_use_while_knocked_out"));
+    public static final TagKey<Block> ALLOW_BREAK_WHILE_KNOCKED_OUT = TagKey.create(Registries.BLOCK, new ResourceLocation(HardcoreRevival.MOD_ID, "allow_break_while_knocked_out"));
+}
