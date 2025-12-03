29cbad3e7fa1cd5e6e4ec68bd89c4550658b70cd
Ryvu1 <247581399+Ryvu1@users.noreply.github.com>
2025-12-03 15:37:25 +0100

    feat: Added stats for timesRescued and playersRevived (#175)
    #173

--- common/src/main/java/net/blay09/mods/hardcorerevival/stats/ModStats.java
+++ common/src/main/java/net/blay09/mods/hardcorerevival/stats/ModStats.java
@@ -8,7 +8,15 @@ public class ModStats {
 
     public static final ResourceLocation knockouts = ResourceLocation.fromNamespaceAndPath(HardcoreRevival.MOD_ID, "knockouts");
 
+    public static final ResourceLocation timesRescued = ResourceLocation.fromNamespaceAndPath(HardcoreRevival.MOD_ID, "times_rescued");
+
+    public static final ResourceLocation playersRevived = ResourceLocation.fromNamespaceAndPath(HardcoreRevival.MOD_ID, "players_revived");
+
     public static void initialize(BalmStats stats) {
         stats.registerCustomStat(knockouts);
+        stats.registerCustomStat(timesRescued);
+        stats.registerCustomStat(playersRevived);
+
     }
+
 }
