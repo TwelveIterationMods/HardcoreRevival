2ac3d523ae3d5aaca4a57c8e726054716703a22e
Ryvu1 <247581399+ryvu1@users.noreply.github.com>
2025-12-03 15:12:11 +0100

    feat: Added stats for timesRescued and playersRevived (#174)
    #173

--- common/src/main/java/net/blay09/mods/hardcorerevival/HardcoreRevivalManager.java
+++ common/src/main/java/net/blay09/mods/hardcorerevival/HardcoreRevivalManager.java
@@ -91,6 +91,7 @@ public class HardcoreRevivalManager {
         PlayerHardcoreRevivalManager.setLastRescuedAt(player, System.currentTimeMillis());
         PlayerHardcoreRevivalManager.setLastKnockoutTicksPassed(player, PlayerHardcoreRevivalManager.getKnockoutTicksPassed(player));
         reset(player);
+        player.awardStat(ModStats.timesRescued);
 
         if (applyEffects) {
             HardcoreRevivalConfig config = HardcoreRevivalConfig.getActive();
@@ -136,6 +137,7 @@ public class HardcoreRevivalManager {
             MinecraftServer server = rescueTarget.level().getServer();
             if (server != null) {
                 wakeup(rescueTarget);
+                player.awardStat(ModStats.playersRevived);
 
                 Balm.networking().sendTo(player, new RevivalProgressMessage(rescueTarget.getId(), -1f));
                 Balm.networking().sendTo(rescueTarget, new RevivalSuccessMessage(rescueTarget.getId()));
--- common/src/main/java/net/blay09/mods/hardcorerevival/stats/ModStats.java
+++ common/src/main/java/net/blay09/mods/hardcorerevival/stats/ModStats.java
@@ -8,8 +8,13 @@ import net.minecraft.stats.StatFormatter;
 public class ModStats {
 
     public static final Identifier knockouts = Identifier.fromNamespaceAndPath(HardcoreRevival.MOD_ID, "knockouts");
+    public static final Identifier timesRescued = Identifier.fromNamespaceAndPath(HardcoreRevival.MOD_ID, "times_rescued");
+    public static final Identifier playersRevived = Identifier.fromNamespaceAndPath(HardcoreRevival.MOD_ID, "players_revived");
 
     public static void initialize(BalmCustomStatRegistrar stats) {
         stats.register(knockouts, StatFormatter.DEFAULT);
+        stats.register(timesRescued, StatFormatter.DEFAULT);
+        stats.register(playersRevived, StatFormatter.DEFAULT);
     }
+
 }
