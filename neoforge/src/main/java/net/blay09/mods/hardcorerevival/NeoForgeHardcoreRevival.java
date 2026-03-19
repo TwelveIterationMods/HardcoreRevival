5ced31377a40328058da0f1403173000890b8e88
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-19 13:42:37 +0100

    feat: Update NeoForge to Minecraft 26.1

--- neoforge/src/main/java/net/blay09/mods/hardcorerevival/NeoForgeHardcoreRevival.java
+++ neoforge/src/main/java/net/blay09/mods/hardcorerevival/NeoForgeHardcoreRevival.java
@@ -3,13 +3,14 @@ package net.blay09.mods.hardcorerevival;
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(HardcoreRevival.MOD_ID)
 public class NeoForgeHardcoreRevival {
 
-    public NeoForgeHardcoreRevival(IEventBus eventBus) {
-        final var context = new NeoForgeLoadContext(eventBus);
+    public NeoForgeHardcoreRevival(ModContainer modContainer, IEventBus eventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, eventBus);
         Balm.initializeMod(HardcoreRevival.MOD_ID, context, HardcoreRevival::initialize);
     }
 
--- neoforge/src/main/java/net/blay09/mods/hardcorerevival/client/NeoForgeHardcoreRevivalClient.java
+++ neoforge/src/main/java/net/blay09/mods/hardcorerevival/client/NeoForgeHardcoreRevivalClient.java
@@ -5,13 +5,14 @@ import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.blay09.mods.hardcorerevival.HardcoreRevival;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(value = HardcoreRevival.MOD_ID, dist = Dist.CLIENT)
 public class NeoForgeHardcoreRevivalClient {
 
-    public NeoForgeHardcoreRevivalClient(IEventBus eventBus) {
-        final var context = new NeoForgeLoadContext(eventBus);
+    public NeoForgeHardcoreRevivalClient(ModContainer modContainer, IEventBus eventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, eventBus);
         BalmClient.initializeMod(HardcoreRevival.MOD_ID, context, HardcoreRevivalClient::initialize);
     }
 }
