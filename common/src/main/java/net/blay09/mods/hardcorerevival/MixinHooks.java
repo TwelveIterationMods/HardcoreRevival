156f509147cdeef34d957de2306ab55f46003bc2
Benjamin E. Iserman <winggar1228@gmail.com>
2026-06-13 00:16:30 -0700

    fix: Fix movement issues when used with Sable (#187)
    * Fix sable compat, resolve #184
    
    * remove commented out code
    
    * remove unused method
    
    * Restore handleMovePlayer mixin to reject incoming movement packets
    
    * Cleanup imports
    
    ---------
    
    Co-authored-by: BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>

--- common/src/main/java/net/blay09/mods/hardcorerevival/MixinHooks.java
+++ common/src/main/java/net/blay09/mods/hardcorerevival/MixinHooks.java
@@ -24,7 +24,7 @@ public class MixinHooks {
     public static void handleProcessPlayerRotation(ServerPlayer player, ServerboundMovePlayerPacket packet) {
         float yaw = packet.getYRot(player.getYRot());
         float pitch = packet.getXRot(player.getXRot());
-        player.absMoveTo(player.getX(), player.getY(), player.getZ(), yaw, pitch);
+        player.absRotateTo(yaw, pitch);
     }
 
     public static boolean shouldCancelToss(Player player, ItemStack itemStack) {
