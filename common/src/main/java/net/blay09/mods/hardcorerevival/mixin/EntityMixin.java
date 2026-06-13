ea74d91307766df10ac4d2b2299f4d0ffaae74ae
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-06-13 09:15:06 +0200

    Cleanup imports

--- common/src/main/java/net/blay09/mods/hardcorerevival/mixin/EntityMixin.java
+++ common/src/main/java/net/blay09/mods/hardcorerevival/mixin/EntityMixin.java
@@ -2,14 +2,9 @@ package net.blay09.mods.hardcorerevival.mixin;
 
 import net.blay09.mods.hardcorerevival.MixinHooks;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.MoverType;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
