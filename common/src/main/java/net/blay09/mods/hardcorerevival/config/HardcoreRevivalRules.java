546e5aa03aae69661fb2cb7219f83578addd72c1
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-01 09:56:09 +0200

    feat: Add `hardcorerevival:revived` shogi rule for finetuning rescue effects

---
+++ common/src/main/java/net/blay09/mods/hardcorerevival/config/HardcoreRevivalRules.java
@@ -0,0 +1,60 @@
+package net.blay09.mods.hardcorerevival.config;
+
+import com.mojang.datafixers.util.Either;
+import net.blay09.mods.hardcorerevival.HardcoreRevival;
+import net.blay09.mods.shogi.Shogi;
+import net.blay09.mods.shogi.ShogiValue;
+import net.blay09.mods.shogi.context.ShogiContext;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.effect.MobEffectInstance;
+import org.jspecify.annotations.Nullable;
+
+import static net.blay09.mods.hardcorerevival.HardcoreRevival.id;
+
+public class HardcoreRevivalRules {
+
+    public static final ShogiValue<ShogiContext, ?> revived = Shogi.maybe(id("revived"), HardcoreRevivalRules::applyDefaultRevivedEffects);
+
+    public static void initialize() {
+    }
+
+    private static Either<Boolean, ?> applyDefaultRevivedEffects(ShogiContext context) {
+        final var player = context.requirePlayer();
+        final var config = HardcoreRevivalConfig.getActive();
+        player.setHealth(config.rescueRespawnHealth);
+        final var foodData = player.getFoodData();
+        foodData.setFoodLevel(Math.min(foodData.getFoodLevel(), config.rescueRespawnFoodLevel));
+        // client only, won't bother: player.getFoodStats().setFoodSaturationLevel((float) config.getRescueRespawnFoodSaturation());
+
+        for (String effectString : config.rescueRespawnEffects) {
+            String[] parts = effectString.split("\\|");
+            Identifier registryName = Identifier.tryParse(parts[0]);
+            if (registryName != null) {
+                final var holder = BuiltInRegistries.MOB_EFFECT.get(registryName);
+                if (holder.isPresent()) {
+                    int duration = tryParseInt(parts.length >= 2 ? parts[1] : null, 600);
+                    int amplifier = tryParseInt(parts.length >= 3 ? parts[2] : null, 0);
+                    player.addEffect(new MobEffectInstance(holder.get(), duration, amplifier));
+                } else {
+                    HardcoreRevival.logger.info("Invalid rescue potion effect '{}'", parts[0]);
+                }
+            } else {
+                HardcoreRevival.logger.info("Invalid rescue potion effect '{}'", parts[0]);
+            }
+        }
+
+        return Either.left(true);
+    }
+
+    private static int tryParseInt(@Nullable String text, int defaultVal) {
+        if (text != null) {
+            try {
+                return Integer.parseInt(text);
+            } catch (NumberFormatException e) {
+                return defaultVal;
+            }
+        }
+        return defaultVal;
+    }
+}
