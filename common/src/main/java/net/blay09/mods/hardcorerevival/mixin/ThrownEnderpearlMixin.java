49e0215a4f251eb98a529554fd64dc510313fd3a
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-26 09:10:20 +0200

    feat: Add `enderPearlsVanishOnKnockout` option (default: true) #197

---
+++ common/src/main/java/net/blay09/mods/hardcorerevival/mixin/ThrownEnderpearlMixin.java
@@ -0,0 +1,21 @@
+package net.blay09.mods.hardcorerevival.mixin;
+
+import net.blay09.mods.hardcorerevival.MixinHooks;
+import net.minecraft.world.entity.projectile.throwableitemprojectile.ThrownEnderpearl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ThrownEnderpearl.class)
+public class ThrownEnderpearlMixin {
+
+    @Inject(method = "tick()V", at = @At("HEAD"), cancellable = true)
+    private void tick(CallbackInfo ci) {
+        final var enderPearl = (ThrownEnderpearl) (Object) this;
+        if (MixinHooks.shouldDiscardEnderPearl(enderPearl, enderPearl.getOwner())) {
+            enderPearl.discard();
+            ci.cancel();
+        }
+    }
+}
