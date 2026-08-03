873ffaf7352307955c0bd56670d9745b5ce7003c
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-08-03 10:21:47 +0200

    fix: Fix swimming up still being possible client-side while knocked out

--- common/src/main/java/net/blay09/mods/hardcorerevival/mixin/LivingEntityMixin.java
+++ common/src/main/java/net/blay09/mods/hardcorerevival/mixin/LivingEntityMixin.java
@@ -1,6 +1,7 @@
 package net.blay09.mods.hardcorerevival.mixin;
 
 import net.blay09.mods.hardcorerevival.HardcoreRevival;
+import net.blay09.mods.hardcorerevival.MixinHooks;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,7 +11,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
 
-    @Inject(method = "Lnet/minecraft/world/entity/LivingEntity;canBeSeenAsEnemy()Z", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "isImmobile()Z", at = @At("HEAD"), cancellable = true)
+    private void isImmobile(CallbackInfoReturnable<Boolean> cir) {
+        final var livingEntity = (LivingEntity) (Object) this;
+        if (MixinHooks.shouldCancelClientMovement(livingEntity)) {
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(method = "canBeSeenAsEnemy()Z", at = @At("HEAD"), cancellable = true)
     private void canBeSeenAsEnemy(CallbackInfoReturnable<Boolean> cir) {
         if (HardcoreRevival.getRevivalData((LivingEntity) (Object) this).isKnockedOut()) {
             cir.setReturnValue(false);
