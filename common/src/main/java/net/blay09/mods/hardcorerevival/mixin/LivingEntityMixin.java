89230a0e6090441f6be69342c697adac44a1ab4e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-08-03 10:21:47 +0200

    fix: Fix swimming up still being possible client-side while knocked out

--- common/src/main/java/net/blay09/mods/hardcorerevival/mixin/LivingEntityMixin.java
+++ common/src/main/java/net/blay09/mods/hardcorerevival/mixin/LivingEntityMixin.java
@@ -1,5 +1,6 @@
 package net.blay09.mods.hardcorerevival.mixin;
 
+import net.blay09.mods.hardcorerevival.MixinHooks;
 import net.blay09.mods.hardcorerevival.PlayerHardcoreRevivalManager;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -10,6 +11,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
+
+    @Inject(method = "isImmobile()Z", at = @At("HEAD"), cancellable = true)
+    private void isImmobile(CallbackInfoReturnable<Boolean> cir) {
+        final var livingEntity = (LivingEntity) (Object) this;
+        if (MixinHooks.shouldCancelClientMovement(livingEntity)) {
+            cir.setReturnValue(true);
+        }
+    }
 
     @Inject(method = "canBeSeenAsEnemy()Z", at = @At("HEAD"), cancellable = true)
     private void canBeSeenAsEnemy(CallbackInfoReturnable<Boolean> cir) {
