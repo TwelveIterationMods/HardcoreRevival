0900910e778afc18b96b4e333417a326194efe0a
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-09-02 19:49:55 +0200

    fix: Fix players still looking like they can be attacked to mobs, possibly fixing some modded mob targeting behavior

--- common/src/main/java/net/blay09/mods/hardcorerevival/mixin/LivingEntityMixin.java
+++ common/src/main/java/net/blay09/mods/hardcorerevival/mixin/LivingEntityMixin.java
@@ -3,6 +3,7 @@ package net.blay09.mods.hardcorerevival.mixin;
 import net.blay09.mods.hardcorerevival.HardcoreRevival;
 import net.blay09.mods.hardcorerevival.MixinHooks;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,6 +23,13 @@ public class LivingEntityMixin {
     @Inject(method = "canBeSeenAsEnemy()Z", at = @At("HEAD"), cancellable = true)
     private void canBeSeenAsEnemy(CallbackInfoReturnable<Boolean> cir) {
         if (HardcoreRevival.getRevivalData((LivingEntity) (Object) this).isKnockedOut()) {
+            cir.setReturnValue(false);
+        }
+    }
+
+    @Inject(method = "canAttack(Lnet/minecraft/world/entity/LivingEntity;)Z", at = @At("HEAD"), cancellable = true)
+    private void canAttack(LivingEntity target, CallbackInfoReturnable<Boolean> cir) {
+        if (target instanceof Player && HardcoreRevival.getRevivalData(target).isKnockedOut()) {
             cir.setReturnValue(false);
         }
     }
